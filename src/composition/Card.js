e51c67fbe8d613c766b25ad98350af1aeb3aae8f
Eli Reiners <goelyukim@gmail.com>
2019-12-16 14:17:23 -0500

    Some updates

---
+++ src/composition/Card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Card.css';
+import { tsPropertySignature } from '@babel/types';
+
+function Card(props){
+    return (
+        <div className="Card">
+            <h3>
+                {props.title}
+            </h3>
+            <p>
+                {props.content}
+            </p>
+        </div>
+    )
+}
+
+export default Card
---
+++ src/composition/List.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './List.css';
+
+function List(props){
+    return (
+        <div className='App-list'>
+        <section className='List'>
+            <header></header>
+        </section>
+        <div className='List-cards'>
+
+        </div>
+        </div>
+    )
+}
+
+export default List
